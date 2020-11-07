2a69dd8e94410e17c29d21b7a791a5ca1279ec75
Shreyas Sathe <47695170+SerraTorr@users.noreply.github.com>
2020-11-07 22:47:24 +0530

    Add files via upload
    constants file for universal constants where ever necessary

---
+++ constants.php
@@ -0,0 +1,15 @@
+
+<?php
+
+//change these constants as per your machine:
+
+ $server = "localhost:8000";
+ $username = "root";
+ $password = "";
+
+ //add anymore constants you want to be universal, throughout all the files:
+
+
+ $name = "Zehar";
+
+?>
