6b238a75c97c83d658d5b0da8b733728f69de918
notwarnite <180010028@iitdh.ac.in>
2020-11-25 14:13:56 +0530

    updated database and database_generator

--- database_generator.php
+++ database_generator.php
@@ -25,21 +25,97 @@
     }
     //creating tables;
 
-    $sql = "create table rooms(room_no int, type varchar, occupancy int, is_occupied int, floor int, price int, image varchar)";
-    if(!mysqli_query($conn, $sql)){
-        die("rooms table creation error");
-    }
-    
-    $sql = "create table customers(cust_id primary key serial, name varchar, age varchar, request_type varchar, request_floor int, checkout_amount int, phone_no varchar address varchar);"
-    if(!mysqli_query($conn, $sql)){
-        die("customers table creation error");
-    }
+    $sql = "CREATE TABLE booking (
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    cid INT(11) NOT NULL,
+    status ENUM (PENDING, CONFIRMED, CANCELLED) NOT NULL DEFAULT Pending,
+    numberOfRooms INT(5) DEFAULT 1,
+    PRIMARY KEY (id)
+    )";
 
-
-    $sql = "create table booking(booking_id primary key serial, cust_id int, room_no int, no_days int,arrival date, check_in time, check_out time,  amount_paid int);"
     if(!mysqli_query($conn, $sql)){
         die("booking table creation error");
     }
+    
+    $sql = "CREATE TABLE customer (
+    cid INT(11) NOT NULL AUTO_INCREMENT,
+    fullname VARCHAR(100) NOT NULL,
+    age VARCHAR(10) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    cardNumber INT(16) DEFAULT NULL,
+    phone VARCHAR(25) DEFAULT NULL,
+    PRIMARY KEY (cid)
+    )";
+
+    if(!mysqli_query($conn, $sql)){
+        die("customer table creation error");
+    }
+
+
+    $sql = "CREATE TABLE pricing (
+    pricing_id INT(6) NOT NULL AUTO_INCREMENT,
+    booking_id INT(6) NOT NULL,
+    nights INT(4) NOT NULL,
+    total_price DOUBLE NOT NULL DEFAULT 0,
+    booked_date DATE NOT NULL,
+    PRIMARY KEY (pricing_id)
+    )";
+
+    if(!mysqli_query($conn, $sql)){
+        die("pricing table creation error");
+    }
+
+    $sql = "CREATE TABLE administrator (
+    admin_Id INT(10) NOT NULL AUTO_INCREMENT,
+    fullname VARCHAR(100) DEFAULT NULL,
+    password VARCHAR(100) NOT NULL,
+    email VARCHAR(30) NOT NULL,
+    PRIMARY KEY (admin_Id)
+    )";
+
+    if(!mysqli_query($conn, $sql)){
+        die("administrator table creation error");
+    }
+
+    $sql = "CREATE TABLE reservation (
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    checkin DATE NOT NULL DEFAULT NULL,
+    checkout DATE NOT NULL,
+    type ENUM (Single, Double, Deluxe, Suite) NOT NULL DEFAULT Single,
+    adults INT(6) NOT NULL,
+    children INT(6) DEFAULT 0,
+    requests VARCHAR(500) DEFAULT NULL,
+    timestamp TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
+    )";
+
+    if(!mysqli_query($conn, $sql)){
+        die("reservation table creation error");
+    }
+
+    $sql = "ALTER TABLE booking
+    ADD CONSTRAINT booking_customer__fk FOREIGN KEY (cid) REFERENCES customer (cid) ON DELETE CASCADE";
+
+    if(!mysqli_query($conn, $sql)){
+        die("alter table failed");
+    }
+
+    $sql = "ALTER TABLE reservation
+    ADD CONSTRAINT reservation_booking__fk FOREIGN KEY (id) REFERENCES booking (id) ON DELETE CASCADE";
+
+    if(!mysqli_query($conn, $sql)){
+        die("alter table failed");
+    }
+
+    $sql = "ALTER TABLE pricing
+    ADD CONSTRAINT pricing_booking__fk FOREIGN KEY (booking_id) REFERENCES booking (id) ON DELETE CASCADE";
+
+    if(!mysqli_query($conn, $sql)){
+        die("alter table failed");
+    }
+
+
         
     die("DATABASE INSTALLATION SUCCESSFUL !!");
     
